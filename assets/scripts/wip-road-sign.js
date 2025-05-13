d91525d1b0ed81a28eec78b8fca0b389d1726c7e
xyfruux <victor.stampe2010@gmail.com>
2025-05-13 21:39:05 +0200

    Create Arrow Input & Validation

--- assets/scripts/wip-road-sign.js
+++ assets/scripts/wip-road-sign.js
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+arrowDir = null;
+
+function setArrowDir() {
+  let rnd = Math.floor(Math.random() * 4);
+  let dir;
+  switch (rnd) {
+    case 0:
+      arrowDir = "ArrowUp";
+      break;
+    case 1:
+      arrowDir = "ArrowDown";
+      break;
+    case 2:
+      arrowDir = "ArrowLeft";
+      break;
+    case 3:
+      arrowDir = "ArrowRight";
+      break;
+  }
+}
+
+setArrowDir();
+console.log(arrowDir);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === arrowDir) {
+    console.log("Correct key pressed!");
+    setArrowDir();
+    console.log(arrowDir);
+  }
+});
