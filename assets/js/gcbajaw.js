a6b24c38dacd37f5c81cabaf247d6ad0b95a3537
Victor <victor.stampe2010@gmail.com>
2025-07-29 20:22:04 +0000

    Create printScreen Function

--- assets/js/gcbajaw.js
+++ assets/js/gcbajaw.js
@@ -1,1 +1,8 @@
-console.log("gcbajaw.js loaded");
+console.log("gcbajaw.js loaded")
+
+function printScreen(arr){
+    for (let i = 0; i < arr.length; i++) {
+        const comment = document.createComment(arr[i])
+        document.body.appendChild(comment)
+    }
+}
