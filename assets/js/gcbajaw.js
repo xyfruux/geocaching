75c2132e3c18c4b070281ad581a200bd97e043b2
Victor <victor.stampe2010@gmail.com>
2025-07-29 20:26:42 +0000

    Add keypress Event & Space Key Detector

--- assets/js/gcbajaw.js
+++ assets/js/gcbajaw.js
@@ -1,5 +1,11 @@
 console.log("gcbajaw.js loaded")
 
+document.addEventListener("keypress", function(e){
+    if(e.key === " "){
+        console.log("Space key pressed")
+    }
+})
+
 function printScreen(arr){
     for (let i = 0; i < arr.length; i++) {
         const comment = document.createComment(arr[i])
