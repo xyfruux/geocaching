8da65a6809ed64f1cc7c95a71b000a4a9abcdc4c
Victor <victor.stampe2010@gmail.com>
2025-07-29 21:17:55 +0000

    Remove Space Key Detector & Add WASD Key Detector, plrPos & Player Movement

--- assets/js/gcbajaw.js
+++ assets/js/gcbajaw.js
@@ -1,9 +1,26 @@
 console.log("gcbajaw.js loaded")
 
+plrPos = [0, 0] // X and Y coordinates
+
 document.addEventListener("keypress", function(e){
-    if(e.key === " "){
-        console.log("Space key pressed")
+    switch(e.key){
+        case "w":
+            plrPos[1] < 10 && (plrPos[1] += 1)
+            break
+        case "s":
+            plrPos[1] > 0 && (plrPos[1] -= 1)
+            break
+        case "a":
+            plrPos[0] > 0 && (plrPos[0] -= 1)
+            break
+        case "d":
+            plrPos[0] < 10 && (plrPos[0] += 1)
+            break
+        default:
+            break
     }
+    console.log(plrPos)
+    console.log(e)
 })
 
 function printScreen(arr){
