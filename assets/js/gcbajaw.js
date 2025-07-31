e3483488b009676aa09cec50dea0fc8752b45d1e
Victor <victor.stampe2010@gmail.com>
2025-07-31 21:28:26 +0000

    Add Semicolons

--- assets/js/gcbajaw.js
+++ assets/js/gcbajaw.js
@@ -1,7 +1,7 @@
-console.log("Du fandt stedet!\nFor at komme videre, skal du fokusere på hjemmesiden (tryk ude fra konsollen, f.eks. på teksten Inspektion) og bruge WASD-tasterne til at bevæge dig rundt.\n\nGod fornøjelse!")
+console.log("Du fandt stedet!\nFor at komme videre, skal du fokusere på hjemmesiden (tryk ude fra konsollen, f.eks. på teksten Inspektion) og bruge WASD-tasterne til at bevæge dig rundt.\n\nGod fornøjelse!");
 
-plrPos = [0, 0] // X and Y coordinates
-btnPos = [Math.floor(Math.random() * 8) + 3, Math.floor(Math.random() * 8) + 3] // Random button position, from 3 to 10 on both axes
+plrPos = [0, 0]; // X and Y coordinates
+btnPos = [Math.floor(Math.random() * 8) + 3, Math.floor(Math.random() * 8) + 3]; // Random button position, from 3 to 10 on both axes
 
 document.addEventListener("keypress", function(e){
     e.key === "w" && plrPos[1] < 10 && plrPos[1]++;
@@ -12,18 +12,18 @@ document.addEventListener("keypress", function(e){
     if(["w", "a", "s", "d"].includes(e.key)){
         printScreen();
 
-        plrPos[0] === btnPos[0] && plrPos[1] === btnPos[1] ? console.log("Du trykkede på knappen!\nHer er koordinaterne:\n[Koordinater]") : null
+        plrPos[0] === btnPos[0] && plrPos[1] === btnPos[1] ? console.log("Du trykkede på knappen!\nHer er koordinaterne:\n[Koordinater]") : null;
     }
 })
 
 function printScreen(){
-    let output = ""
+    let output = "";
     for(let y = 10; y >= 0; y--){
         for(let x = 0; x <= 10; x++){
             output += x === plrPos[0] && y === plrPos[1] ? "🥳" : x === btnPos[0] && y === btnPos[1] ? "🔴" : "⬛";
 
         }
-        output += "\n"
+        output += "\n";
     }
-    console.log(output)
+    console.log(output);
 }
